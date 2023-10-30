4592b06710292984ff8213b4686f2134605a3df8
Thiago Santos <thiagop.santos182@gmail.com>
2023-10-30 11:31:35 -0400

    feat: mapping N-N with association class part 2

--- src/main/java/com/devsuperior/dscommerce/entities/Order.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Order.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import java.time.Instant;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -79,5 +80,9 @@ public class Order {
 
     public Set<OrderItem> getItems() {
         return items;
+    }
+
+    public List<Product> getProducts() {
+        return items.stream().map(OrderItem::getProduct).toList();
     }
 }
--- src/main/java/com/devsuperior/dscommerce/entities/Product.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Product.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -83,5 +84,9 @@ public class Product {
 
     public Set<OrderItem> getItems() {
         return items;
+    }
+
+    public List<Order> getOrders() {
+        return items.stream().map(orderItem -> orderItem.getOrder()).toList();
     }
 }
